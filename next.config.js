e9ecce36969e3853084eb101dd00cfd194237f8d
Estefanía García <estefania.garcia@mataro.epiaedu.cat>
2025-06-27 00:56:46 +0200

    changes csp 2

--- next.config.js
+++ next.config.js
@@ -1,25 +1,23 @@
-// next.config.js
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)', // aplica para todas las rutas
+        source: '/(.*)', // todas las rutas
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-inline' https://*.spotify.com https://*.google.com https://*.googletagmanager.com https://*.google-analytics.com;
-              connect-src 'self' https://*.spotify.com https://*.google.com https://*.googletagmanager.com https://*.google-analytics.com https://*.ingest.sentry.io;
-              img-src 'self' data: https://*.spotify.com https://*.google.com;
-              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-              font-src 'self' https://fonts.gstatic.com;
-              frame-src https://accounts.spotify.com https://www.google.com;
-            `.replace(/\n/g, ' ')
-          }
-        ]
-      }
-    ]
-  }
-}
-// Configuración de Next.js para manejar CSP y otros headers
+            value: "default-src 'self'; " +
+                   "script-src 'self' https://*.spotify.com https://*.google-analytics.com https://www.google.com/recaptcha/enterprise; " +
+                   "connect-src 'self' https://*.spotify.com https://*.google-analytics.com https://www.google.com/recaptcha/enterprise; " +
+                   "img-src 'self' data: https://*.spotify.com; " +
+                   "style-src 'self' 'unsafe-inline'; " +
+                   "font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com;",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
